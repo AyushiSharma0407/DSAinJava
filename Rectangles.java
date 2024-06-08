f4f21a72ed6713d7a34d7e28fe20e28857136bd7
AyushiSharma0407 <124056704+AyushiSharma0407@users.noreply.github.com>
2024-06-08 12:16:30 +0530

    Create Rectangles.java

---
+++ Rectangles.java
@@ -0,0 +1,21 @@
+//Hollow Rectangle 
+for(int i=1;i<=4;i++){
+            for(int j=1;j<=5;j++){
+                if(i==1||i==4||j==1||j==5){
+
+                    System.out.print("*");
+                } else{
+                    System.out.print(" ");
+                }
+            }
+        System.out.println();
+       }
+
+
+//Solid Rectangle
+for(int i=1;i<=4;i++){
+            for(int j=1; j<=10; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
