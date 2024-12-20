b7dae60384ac936407f9177cff9f5b880b20489e
AyushiSharma0407 <ayushis2018@gmail.com>
2024-12-20 22:49:29 +0530

    Create StringCompression.java

---
+++ Strings/StringCompression.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class StringCompression{
+    //O(n) because while is also doing i++
+    public static void main(String args[]){
+        String str = "aaaabbbbc";
+        System.out.println(compress(str));
+    }
+
+    public static String compress(String str){
+        String newStr ="";
+
+        for(int i=0; i<str.length();i++){
+            Integer count = 1;
+            while(i<str.length()-1 && str.charAt(i)==str.charAt(i+1)){
+                count++;
+                i++;
+            }
+            newStr += str.charAt(i);
+            if(count >1){
+                newStr += count.toString();
+            }
+        }
+        return newStr;
+    }
+}
