099294b238726751c988a3aff2aa5c2a24868ac2
AyushiSharma0407 <ayushis2018@gmail.com>
2024-12-23 22:36:20 +0530

    Create OddEven.java

---
+++ Bits/OddEven.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class OddEven{
+    public static void main(String args[]){
+        OddorEven(3);
+        OddorEven(44);
+
+    }
+
+    public static void OddorEven(int n){
+        int bitmask=1;
+        if((n&bitmask)==0){
+            System.out.println("even");
+        }else{
+            System.out.println("odd");
+        }
+    }
+}
